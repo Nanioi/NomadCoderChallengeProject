85cba6b3dce4e7f56bae2227c8712692d5ffff73
Nanioi <0802skdus@naver.com>
2024-09-03 14:33:35 +0900

    nomadcoder flutter challenge 코드 개선

--- toonflix/lib/widgets/currency_card.dart
+++ toonflix/lib/widgets/currency_card.dart
@@ -4,6 +4,7 @@ class CurrencyCard extends StatelessWidget {
   final String name, code, amount;
   final IconData icon;
   final bool isInverted;
+  final double index;
 
   final _blackColor = const Color(0xFF1F2123);
   const CurrencyCard({
@@ -13,70 +14,74 @@ class CurrencyCard extends StatelessWidget {
     required this.amount,
     required this.icon,
     required this.isInverted,
+    required this.index,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge, // 넘어가는 모든 것 자르기, default는 none (넘쳐도 괜찮다)
-      decoration: BoxDecoration(
-        color: isInverted ? Colors.white : _blackColor,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                      color: isInverted ? _blackColor : Colors.white,
-                      fontSize: 32),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
-                          color: isInverted ? _blackColor : Colors.white,
-                          fontSize: 18),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      code,
-                      style: TextStyle(
-                          color: isInverted
-                              ? _blackColor
-                              : Colors.white.withOpacity(0.8),
-                          fontSize: 20),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            Transform.scale(
-              // icon 크기 변경
-              scale: 2.2, // 원래 크기의 2.2배로 변경, 부모에게 영향X
-              child: Transform.translate(
-                // icon 위치 이동
-                offset: const Offset(-5, 12),
-                child: Icon(
-                  icon,
-                  color: isInverted ? _blackColor : Colors.white,
-                  size: 88,
+    return Transform.translate(
+      offset: Offset(0, (-20 * index)),
+      child: Container(
+        clipBehavior: Clip.hardEdge, // 넘어가는 모든 것 자르기, default는 none (넘쳐도 괜찮다)
+        decoration: BoxDecoration(
+          color: isInverted ? Colors.white : _blackColor,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                        color: isInverted ? _blackColor : Colors.white,
+                        fontSize: 32),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                            color: isInverted ? _blackColor : Colors.white,
+                            fontSize: 18),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        code,
+                        style: TextStyle(
+                            color: isInverted
+                                ? _blackColor
+                                : Colors.white.withOpacity(0.8),
+                            fontSize: 20),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Transform.scale(
+                // icon 크기 변경
+                scale: 2.2, // 원래 크기의 2.2배로 변경, 부모에게 영향X
+                child: Transform.translate(
+                  // icon 위치 이동
+                  offset: const Offset(-5, 12),
+                  child: Icon(
+                    icon,
+                    color: isInverted ? _blackColor : Colors.white,
+                    size: 88,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
