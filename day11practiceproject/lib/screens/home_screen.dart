df4431973330524c624c1695aef30f00e8db0a04
Nanioi <0802skdus@naver.com>
2024-09-05 09:12:33 +0900

    Day11 5.1강의 버튼 누르면 1초마다 카운트되는 기능 추가

--- day11practiceproject/lib/screens/home_screen.dart
+++ day11practiceproject/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:day11practiceproject/core/theme/constant/app_colors.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +11,24 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
+  void onStartPressed() {
+    // 원하는 주기마다 다음 콜백 함수 실행
+    // 1초마다
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                '25:00',
+                '$totalSeconds',
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 89,
@@ -36,7 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
                 onPressed: () {
-                  //todo
+                  onStartPressed();
                 },
                 icon: const Icon(
                   Icons.play_circle_outline,
@@ -51,8 +71,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 Expanded(
                   child: Container(
                     decoration: BoxDecoration(
-                      color: Theme.of(context).cardColor,
-                    ),
+                        color: Theme.of(context).cardColor,
+                        borderRadius: BorderRadius.circular(50)),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
