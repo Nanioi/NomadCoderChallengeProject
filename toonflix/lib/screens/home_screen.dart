c9aca8eed2dbe629aae68c6614f6040b127e8971
Nanioi <0802skdus@naver.com>
2024-09-06 16:29:26 +0900

    Day12 6.7 ListView

--- toonflix/lib/screens/home_screen.dart
+++ toonflix/lib/screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +26,19 @@ class HomeScreen extends StatelessWidget {
         builder: (context, snapshot) {
           //snapshot을 통해 future의 상태를 알 수 있다.
           if (snapshot.hasData) {
-            return const Text("There is Data");
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
+            );
           }
-          return const Text('Loading......');
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
